2621a206c71cd64b28f75d953cc341f5d47ae388
Sade-Tuuli <tuulei_@hotmail.com>
2018-12-04 23:44:21 +0200

    pom fix

--- Miinaharava/src/main/java/guifx/ImageHandler.java
+++ Miinaharava/src/main/java/guifx/ImageHandler.java
@@ -17,20 +17,20 @@ public class ImageHandler {
 
     public ImageHandler() {
         images = new Image[14];
-        images[0] = new Image("file:src\\main\\resources\\exposed.png");
-        images[1] = new Image("file:src\\main\\resources\\number1.png");
-        images[2] = new Image("file:src\\main\\resources\\number2.png");
-        images[3] = new Image("file:src\\main\\resources\\number3.png");
-        images[4] = new Image("file:src\\main\\resources\\number4.png");
-        images[5] = new Image("file:src\\main\\resources\\number5.png");
-        images[6] = new Image("file:src\\main\\resources\\number6.png");
-        images[7] = new Image("file:src\\main\\resources\\number7.png");
-        images[8] = new Image("file:src\\main\\resources\\number8.png");
-        images[9] = new Image("file:src\\main\\resources\\blank.png");
-        images[10] = new Image("file:src\\main\\resources\\flag.png");
-        images[11] = new Image("file:src\\main\\resources\\mine.png");
-        images[12] = new Image("file:src\\main\\resources\\hitmine.png");
-        images[13] = new Image("file:src\\main\\resources\\wrongmine.png");
+        images[0] = new Image("/exposed.png");
+        images[1] = new Image("/number1.png");
+        images[2] = new Image("/number2.png");
+        images[3] = new Image("/number3.png");
+        images[4] = new Image("/number4.png");
+        images[5] = new Image("/number5.png");
+        images[6] = new Image("/number6.png");
+        images[7] = new Image("/number7.png");
+        images[8] = new Image("/number8.png");
+        images[9] = new Image("/blank.png");
+        images[10] = new Image("/flag.png");
+        images[11] = new Image("/mine.png");
+        images[12] = new Image("/hitmine.png");
+        images[13] = new Image("/wrongmine.png");
     }
 
     public Image setImage(int x) {
